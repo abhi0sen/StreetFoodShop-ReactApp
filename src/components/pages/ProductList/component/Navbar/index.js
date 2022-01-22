4d1920fdea1ca7ffeedaf430f34dd9475441624b
Abhisar Sen <senabhisar@gmail.com>
2022-01-22 10:13:49 +0530

    Workshop-Day3

--- src/components/pages/ProductList/component/Navbar/index.js
+++ src/components/pages/ProductList/component/Navbar/index.js
@@ -2,10 +2,10 @@ import "./style.css";
 
 const Navbar = (props) => (
     <div className="navbar-container">
-        <img src="/back.png" alt="back" />
+        <img src="./back.png" alt="back" />
         <h1>Search Product</h1>
         <div className="cart-icon">
-            <img src="/bag.png" alt="cart" />
+            <img src="./bag.png" alt="cart" />
             <div className="cart-count">{props.count}</div>
         </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -20,5 +20,3 @@ ReactDom.render(
     </BrowserRouter>,
   document.getElementById('root')
 );
-
-
