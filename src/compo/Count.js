370904890e073563f95e0deea576ebeb7a55f711
Abhisar Sen <senabhisar@gmail.com>
2022-01-23 10:17:48 +0530

    practice start

---
+++ src/compo/Count.js
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+// import { useEffect } from "react";
+
+class FShop{
+    constructor(name) {
+        this.brand = name;
+    };
+    
+}
+// const FoodShop = new FShop("Street food");
+
+export default FShop;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import Heading from './components/Heading';
-import Count from './components/Count';
-import CountComponent from './components/countcClass';
-import Loop from './components/loop';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
 
 
-ReactDom.render(
-  <div>
-    <Heading title = "Hello Props" />
-    <Count initialValue = {5} />
-    <hr />
-  <CountComponent  initialValue = {10} />
-  <Loop />
-  </div>,
-  document.getElementById('root')
-
-);
 
